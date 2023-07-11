a28605a8b4f31c2fe4c7c2db2948ee3028378460
Marrrriys <139171663+Marrrriys@users.noreply.github.com>
2023-07-11 15:20:10 +0300

    from GitHub

--- src/Main/Java/ua/hillel/sereda/homeworks/homework3/Main3.java
+++ src/Main/Java/ua/hillel/sereda/homeworks/homework3/Main3.java
@@ -2,6 +2,6 @@ package ua.hillel.sereda.homeworks.homework3;
 
 public class Main3 {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello GitHub");
     }
 }
