5520f64064847cc5cd2ee7405d98baa2cb6e21f4
Sereda Marina <Levanevskogo1257@gmail.com>
2023-07-12 15:52:54 +0300

    test commit IDEA4

---
+++ src/Main/Java/ua/hillel/sereda/homeworks/homework4/Main4.java
@@ -0,0 +1,16 @@
+package ua.hillel.sereda.homeworks.homework4;
+
+public class Main4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int c = 5;
+
+         int volume = a * b * c;
+         int length = a + b + c;
+
+        System.out.println("volume = " + volume);
+        System.out.println(" length = " +  length);
+
+    }
+}
